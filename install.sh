dbb1eaaf219b14c5aa9e62e764ca2ab6a65a9501
Ratul Maharaj <ratulmaharaj@gmail.com>
2021-04-19 20:27:58 +0200

    reorder

--- install.sh
+++ install.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-# move files into correct directory
-echo "moving config files to the correct directory"
-cp "./.zshrc" "${HOME}/.zshrc"
-cp "./.zsh_aliases" "${HOME}/.zsh_aliases"
-cp "./.p10k.zsh" "${HOME}/.p10k.zsh"
+
 
 # install oh-my-zsh
 # remove existing installation
@@ -15,5 +11,11 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 # install powerlevel10k
 echo "Installing powerlevel10k theme"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# move files into correct directory
+echo "moving config files to the correct directory"
+cp "./.zshrc" "${HOME}/.zshrc"
+cp "./.zsh_aliases" "${HOME}/.zsh_aliases"
+cp "./.p10k.zsh" "${HOME}/.p10k.zsh"
 
 echo "installation complete!"
