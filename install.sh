34ea6e60301d89794423380491ac6b9dab1c0755
Ratul Maharaj <ratulmaharaj@gmail.com>
2021-04-19 20:56:12 +0200

    Install plugins

--- install.sh
+++ install.sh
@@ -10,6 +10,15 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 echo "Installing powerlevel10k theme"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
+# install plugins
+echo "Installing plugins"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+
+
+
 # move files into correct directory
 echo "moving config files to the correct directory"
 cp "./.zshrc" "${HOME}/.zshrc"
