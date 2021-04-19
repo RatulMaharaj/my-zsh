c4ac2510392ab58ddd6d573200401564df09c079
Ratul Maharaj <ratulmaharaj@gmail.com>
2021-04-19 19:54:54 +0200

    added steps to installation

--- install.sh
+++ install.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
 # install oh-my-zsh
-echo "Installing oh-my-zsh"
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+if [-d $ZSH]; then 
+    echo "Installing oh-my-zsh"
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+else
+    echo "oh-my-zsh installation already found."
+fi
 
 # install powerlevel10k
 echo "Installing powerlevel10k theme"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# move files into correct directory
+echo "moving config files to the correct directory"
+mv "./.zshrc" "${HOME}/.zshrc"
+mv "./.zsh_aliases" "${HOME}/.zsh_aliases"
+mv "./.p10k.zsh" "${HOME}/.p10k.zsh"
+
+echo "installation complete!"
