4f0094ed44429701e7be63acd96783ea34c47494
Ratul Maharaj <ratulmaharaj@gmail.com>
2021-04-19 19:45:18 +0200

    added install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# install oh-my-zsh
+echo "Installing oh-my-zsh"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# install powerlevel10k
+echo "Installing powerlevel10k theme"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
